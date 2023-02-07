10c891a79a7cba5bfd6ac7c50a9774ae44f50f87
chaeeun-Han <chaeeun.h911@gmail.com>
2023-02-07 12:03:41 +0900

    MDS-19 feat: Load public data to local

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+// env
+const dotenv = require("dotenv");
+dotenv.config();
+
+const Search = require("./routes/Search");
+
+app.use("/search", Search);
+
+const PORT = 5001;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
