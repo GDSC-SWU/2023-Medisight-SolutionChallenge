f4f654506b9db35623c1ce0892bd0fd09c8c0612
Queue-ri <qriositylog@gmail.com>
2023-03-26 10:54:45 +0900

    MDS-84 fix: Restore missing backend scheduler

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ app.use(express.json());
 const dotenv = require("dotenv");
 dotenv.config();
 
+const database = require("./util/Database");
+
 const Search = require("./routes/Search");
 const Map = require("./routes/Map");
 
