51b8e61b0fc3150012dd932c17d80149e021b01a
chaeeun-Han <chaeeun.h911@gmail.com>
2023-03-30 16:35:34 +0900

    MDS-102 feat: Modify info screen ui

--- medisight/lib/screen/info_screen.dart
+++ medisight/lib/screen/info_screen.dart
@@ -1,119 +1,256 @@
 import 'package:flutter/material.dart';
+import 'package:medisight/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 import '../page/tip_sub_1.dart';
 import '../page/tip_sub_2.dart';
 import '../page/tip_sub_3.dart';
 import '../page/tip_sub_4.dart';
+
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InfoScreen extends StatelessWidget {
   const InfoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeMode =
+        Provider.of<ThemeProvider>(context, listen: false).themeMode;
+    late FlutterTts tts = FlutterTts();
+    tts.speak('상식 페이지');
     return Scaffold(
       appBar: AppBar(
-          title: Image.asset('assets/images/medisight_logo.png',
-              width: 200, height: 200)),
+          title: themeMode == ThemeMode.light
+              ? Image.asset('assets/images/logo-title-light.png',
+                  width: 200, height: 200)
+              : Image.asset('assets/images/logo-title-dark.png',
+                  width: 200, height: 200)),
       body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Text(
-                '상식 페이지',
-                style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(height: 30), // 여백
+            // 상식 버튼
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).canvasColor,
+                fixedSize: const Size(355, 140),
+                //버튼을 둥글게 처리
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
               ),
-              const SizedBox(height: 10),
-              const Text(
-                '원하시는 주제를 선택하세요',
-                style: TextStyle(fontSize: 20, letterSpacing: 2.0),
+              onPressed: _launchUrl,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '더 많은\n정보를 얻고싶다면?',
+                        style: TextStyle(
+                          color: themeMode == ThemeMode.light
+                              ? Colors.black
+                              : Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        '의약품안전나라로 이동',
+                        style: TextStyle(
+                          color: themeMode == ThemeMode.light
+                              ? Colors.black
+                              : Colors.white,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Image.asset('assets/images/img_store.png',
+                      width: 150, height: 160),
+                ],
               ),
-              const SizedBox(height: 30),
-              // 의약품 보관 버튼
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  minimumSize: const Size(350, 80),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-                child: const Text(
-                  "의약품 보관하기",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => const TipSubPage1()),
-                  );
-                },
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              '원하시는 주제를 선택하세요',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
               ),
-              const SizedBox(height: 30), // 여백
-              // 의약품 복용 버튼
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  minimumSize: const Size(350, 80),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
+            ),
+
+            const SizedBox(height: 30), // 여백
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // 의약품 보관 버튼
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: themeMode == ThemeMode.light
+                        ? Colors.blue.shade50
+                        : Theme.of(context).canvasColor,
+                    foregroundColor: Colors.black,
+                    fixedSize: const Size(170, 160),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(Icons.add_box,
+                          size: 40, color: Color.fromARGB(255, 107, 134, 255)),
+                      Text(
+                        "의약품 보관하기",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: themeMode == ThemeMode.light
+                              ? Colors.black
+                              : Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const TipSubPage1()),
+                    );
+                  },
                 ),
-                child: const Text(
-                  "의약품 복용하기",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
+
+                const SizedBox(width: 15), // 여백
+                // 의약품 복용 버튼
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: themeMode == ThemeMode.light
+                        ? Colors.blue.shade50
+                        : Theme.of(context).canvasColor,
+                    foregroundColor: Colors.black,
+                    fixedSize: const Size(170, 160),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(Icons.food_bank,
+                          size: 40, color: Color.fromARGB(255, 107, 134, 255)),
+                      Text(
+                        "의약품 복용하기",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: themeMode == ThemeMode.light
+                              ? Colors.black
+                              : Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const TipSubPage2()),
+                    );
+                  },
                 ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => const TipSubPage2()),
-                  );
-                },
-              ),
-              const SizedBox(height: 30), // 여백
-              // 증상별 의약품 버튼
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  minimumSize: const Size(350, 80),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
+              ],
+            ),
+            const SizedBox(height: 15), // 여백
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // 증상별 의약품 버튼
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: themeMode == ThemeMode.light
+                        ? Colors.blue.shade50
+                        : Theme.of(context).canvasColor,
+                    foregroundColor: Colors.black,
+                    fixedSize: const Size(170, 160),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(Icons.medication,
+                          size: 40, color: Color.fromARGB(255, 107, 134, 255)),
+                      Text(
+                        "증상별 의약품",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: themeMode == ThemeMode.light
+                              ? Colors.black
+                              : Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const TipSubPage3()),
+                    );
+                  },
                 ),
-                child: const Text(
-                  "증상별 의약품",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
+                const SizedBox(width: 15), // 여백
+
+                // 의약품 성분 풀이 버튼
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: themeMode == ThemeMode.light
+                        ? Colors.blue.shade50
+                        : Theme.of(context).canvasColor,
+                    foregroundColor: Colors.black,
+                    fixedSize: const Size(170, 160),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(Icons.medical_information,
+                          size: 40, color: Color.fromARGB(255, 107, 134, 255)),
+                      Text(
+                        "의약품 성분 풀이",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: themeMode == ThemeMode.light
+                              ? Colors.black
+                              : Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const TipSubPage4()),
+                    );
+                  },
                 ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => const TipSubPage3()),
-                  );
-                },
-              ),
-              const SizedBox(height: 30), // 여백
-              // 의약품 성분 풀이 버튼
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  minimumSize: const Size(350, 80),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-                child: const Text(
-                  "의약품 성분 풀이",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => const TipSubPage4()),
-                  );
-                },
-              ),
-            ],
-          ),
+              ],
+            )
+          ],
         ),
       ),
     );
+  }
+
+  Future<void> _launchUrl() async {
+    Uri _url = Uri.parse('https://nedrug.mfds.go.kr/cntnts/34');
+    if (!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
   }
 }
--- medisight/lib/widget/grid_button_1.dart
+++ medisight/lib/widget/grid_button_1.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:medisight/page/tip_content.dart';
+import 'package:medisight/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class GridButton1 extends StatelessWidget {
   final List<Map<String, dynamic>> gridMap = [
     {"title": "포장지와 함께 보관하기"},
     {"title": "유효기간 주의사항"},
     {"title": "어린이 상비약 안전마개"},
-    {"title": "인공눈물 개봉 주의사항"}
+    {"title": "인공눈물 개봉 주의사항"},
+    {"title": "보관용기 종류"},
+    {"title": "제형별 보관법"},
+    {"title": "올바른 의약품 폐기법"},
+    {"title": "종류별 사용기한"},
+    {"title": "약 상자 정리 팁"},
   ];
 
   @override
   Widget build(BuildContext context) {
+    final themeMode =
+        Provider.of<ThemeProvider>(context, listen: false).themeMode;
     return GridView.builder(
       physics: const NeverScrollableScrollPhysics(),
       shrinkWrap: true,
@@ -33,7 +42,14 @@ class GridButton1 extends StatelessWidget {
               borderRadius: BorderRadius.circular(
                 24.0,
               ),
-              color: Colors.grey.shade300,
+              color: themeMode == ThemeMode.light
+                  ? Colors.blue.shade50
+                  : Theme.of(context).canvasColor,
+              border: Border.all(
+                  width: 3.0,
+                  color: themeMode == ThemeMode.light
+                      ? Colors.blue.shade50
+                      : Color.fromARGB(255, 255, 214, 0)),
             ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -47,8 +63,11 @@ class GridButton1 extends StatelessWidget {
                       Text(
                         "${gridMap.elementAt(index)['title']}",
                         style: Theme.of(context).textTheme.subtitle1!.merge(
-                              const TextStyle(
-                                fontSize: 20,
+                              TextStyle(
+                                color: themeMode == ThemeMode.light
+                                    ? Colors.black
+                                    : Colors.white,
+                                fontSize: 16,
                                 fontWeight: FontWeight.w700,
                               ),
                             ),
